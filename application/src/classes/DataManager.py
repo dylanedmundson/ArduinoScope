3035fac4d721a9adf5a07875b0113531f4de4112
dylan.edmundson <dylanedmundson63@gmail.com>
2024-12-22 18:48:54 -0800

    Unit tests complete for DataManager, DataManager shell made

--- application/src/classes/DataManager.py
+++ application/src/classes/DataManager.py
@@ -1,2 +1,50 @@
-def test1():
-    return 1
+import numpy as np
+
+class DataManager:
+    """ This class is responsible for managing data inputs to easily
+        ouput the most recently entered data first (last in first out)
+    """
+    __BUFFER_DEFAULT_LENGTH = 1000
+    __INIT_VALUE = -1
+    
+    def __init__(self):
+        self._time_buffer = np.full(self.__BUFFER_DEFAULT_LENGTH, self.__INIT_VALUE)
+        self.__voltage_buffer = np.full(self.__BUFFER_DEFAULT_LENGTH, self.__INIT_VALUE)
+        self.__pointer = 0 # initialize to buffer start
+        self.__size = 0
+    
+    def add_data(self, time, voltage):
+        """ adds oscilloscope time and voltage data to the manager
+        
+        Args:
+            time_value (int): time data value in micro seconds
+            voltage (float): voltage data value in volts
+        """
+        return
+    
+    
+    def clear_all(self):
+        """ empties the entire array reinitializing to INIT_VALUE
+        """
+        return
+    
+    def pop(self):
+        """ pops most recent value and removes from manager
+        
+            Returns:
+                tuple: A tupple containing the time and voltage
+                    - time (int): associated data time
+                    - voltage (float): associated data voltage
+                    
+            Raises:
+                ValueError in case of empty manager
+        """
+        return -1, -1
+    
+    def data_waiting(self):
+        """ returns the number of data points avaialable for popping
+
+            Returns:
+                size (int): number of data points available
+        """
+        return -1
--- application/test/test_DataManager.py
+++ application/test/test_DataManager.py
@@ -3,7 +3,9 @@ import sys
 
 sys.path.append('C:/Users/dylan/VsCodeRepos/ArduinoScope/application')
 
-from src.classes.DataManager import test1
+import src.classes.DataManager as data_mngr
+
+MAX_BUFFER = data_mngr.DataManager._DataManager__BUFFER_DEFAULT_LENGTH
 
 class TestDataManager(unittest.TestCase):
     
@@ -15,7 +17,107 @@ class TestDataManager(unittest.TestCase):
     def tearDownClass(cls):
         return super().tearDownClass()
     
+    def assert_add_pop_match(self, t_input, v_input, mngr):
+        """ assertion to confirm most resent data add/pop
+            function work properly
+            Args: 
+                t (int): time value
+                v (float): voltage value
+                mngr (data_mngr.DataManager): data manager
+        """
+        if (isinstance(mngr, data_mngr.DataManager)):
+            mngr.add_data(t_input, v_input)
+            t, v = mngr.pop()
+            self.assertEqual(t, t_input)
+            self.assertEqual(v, v_input)
+        else:
+            print("Test code error - assert_add_pop_match")
+    
+    def test_add_data_and_pop(self):
         
+        mnger = data_mngr.DataManager()
+        
+        # test negative time when empty
+        try:
+            mnger.pop()
+            self.assertTrue(False)
+        except ValueError:
+            self.assertTrue(True)
+        
+        self.assert_add_pop_match(100, 5.0, mnger)
+        
+        # test filling buffer to maximum
+        fail_flag = False
+        for i in range(0, MAX_BUFFER):
+            mnger.add_data(i, 5.0)
+        
+        # test proper pointer adjustment and pop/add 
+        # functionality-full buffer
+        mnger.add_data(10285, 2.5)
+        self.assertEqual(mnger.__pointer, 0)
+        t, v = mnger.pop()
+        self.assertEqual(t, 10285)
+        self.assertEqual(v, 2.5)
+        
+        # test poping buffer empty
+        for i in range(MAX_BUFFER, 0):
+            t, v = mnger.pop()
+            if not t == i:
+                fail_flag = True
+        self.assertFalse(fail_flag)
+
+    
+    def clear_all_assisstant(self, mngr):
+        """ assists test_clear_all function to make sure
+            proper functionallity after clearing
+        """
+        if (isinstance(mngr, data_mngr.DataManager)):
+            mngr.clear_all()
+            self.assertEqual(mngr.data_waiting(), 0)
+            try:
+                t,v = mngr.pop()
+                self.assertFalse(True)
+            except ValueError:
+                self.assertTrue(True)
+        else:
+            print("Test code error - test_clear_all_assisstant")
+        
+    def test_clear_all(self):
+        mnger = data_mngr.DataManager()
+
+        # clear all on empty
+        self.clear_all_assisstant(mngr=mnger)
+        
+        #fill completely
+        for i in range(0, MAX_BUFFER):
+            mnger.add_data(i, 5.0)
+        self.clear_all_assisstant(mngr=mnger)
+        
+        # fill one over max length
+        for i in range(0, MAX_BUFFER + 1):
+            mnger.add_data(i, 5.0)
+        self.clear_all_assisstant(mngr=mnger)
+    
+    def test_data_waiting(self):
+        mnger = data_mngr.DataManager()
+        mnger.__init__()
+        
+        # test zero at init
+        self.assertEqual(mnger.data_waiting(), 0)
+        
+        # test increments with filling
+        for i in range(0, MAX_BUFFER):
+            mnger.add_data(1, 1)
+            self.assertEqual(mnger.data_waiting(), i + 1)
+        
+        # test does not increase past buffer max
+        mnger.add_data(1, 1)
+        self.assertEqual(mnger.data_waiting(), MAX_BUFFER)
+        
+        # test returns to zero when cleared
+        mnger.clear_all()
+        self.assertEqual(mnger.data_waiting(), 0)
+            
         
 if __name__ == '__main__':
     unittest.main()
